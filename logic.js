cd0f0d49ff07c1f3eeba988b84f9e658cc431a03
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-16 17:46:52 +0000

    updated index and logic files

--- logic.js
+++ logic.js
@@ -1,1 +1,71 @@
+let warrantyHeader = document.createElement("h2")
+warrantyHeader.textContent = "Warranty Information"
+document.body.append(warrantyHeader)
 
+let firstName = document.createElement("input")
+firstName.textContent = "First Name"
+document.body.append(firstName)
+
+let lastName = document.createElement("imput")
+lastName.textContent = "Last Name"
+document.body.append(lastName)
+
+let comapnyName = document.createElement("input")
+comapnyName.textContent = "Company Name"
+document.body.append(companyName)
+
+let email = document.createElement("input")
+email.textContent = "Email"
+document.body.append(email)
+
+let phoneNumber = document.createElement("input")
+phoneNumber.textContent = "Phone Number"
+document.body.append(phoneNumber)
+
+let faxNumber = document.createElement("input")
+faxNumber.textContent = "Fax Number"
+document.body.append(faxNumber)
+
+let address = document.createElement("input")
+address.textContent = "Address"
+document.body.append(address)
+
+let city = document.createElement("input")
+city.textContent = "City"
+document.body.append(city)
+
+let state = document.createElement("input")
+state.textContent = "State"
+document.body.append(state)
+
+let zipCode = document.createElement("input")
+zipCode.textContent = "Zip Code"
+document.body.append(zipCode)
+
+let country = document.createElement("imput")
+country.textContent = "Country"
+document.body.append(country)
+
+let deviceInformation = document.createElement("h2")
+deviceInformation.textContent = "Device Information"
+document.body.append(deviceInformation)
+
+let dateOfPurchase = document.createElement("input")
+dateOfPurchase.textContent = "Date of Purchase"
+document.body.append(dateOfPurchase)
+
+let model = document.createElement("input")
+model.textContent = "Model"
+document.body.append(model)
+
+let warrantySerialNumber = document.createElement("input")
+warrantySerialNumber.textContent = "Warranty Serial Number"
+document.body.append(warrantySerialNumber)
+
+let save = document.createElement("button")
+save.textContent = "Save"
+document.body.append(save)
+
+let close = document.createElement("button")
+close.textContent = "Close"
+document.body.append(close)
